be5646ef5fe2bcabcd96b2bd2caf9e6dbec57e81
Rooarii <rooarii.manuel@gmail.com>
2020-10-23 07:39:41 +0200

    ajawx request completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import DisplayQuote from './components/DisplayQuote';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// API entry point
+const url = 'https://thesimpsonsquoteapi.glitch.me/quotes?count=5'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+    this.getQuotes = this.getQuotes.bind(this);
+  }
+  // call the getQuotes method at first client render
+  componentDidMount(){
+    this.getQuotes();
+  }
+  getQuotes(){
+    // AJAX request
+    axios.get(url)
+      // check Ajax request answer in console
+      .then(response => console.log("datas from API: ",response.data))
+  }
+  render() {
+    return (
+      // <DisplayQuote quotes={this.getQuotes}/>
+      <>
+      </>
+    );
+  }
 }
 
 export default App;
+
+
+// [
+//   {
+//    "quote": "Shoplifting is a victimless crime, like punching someone in the dark.",
+//    "character": "Nelson Muntz",
+//    "image" : "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
+//    "characterDirection" : "Left"
+//   }
+// ]
+
+// // Feel free to download this HTML and edit it, to use another Pokemon ID
+// const quoteId = 1;
+// const url = `https://simpsons-quotes-api.herokuapp.com/quotes?count=${quoteId}`;
+// axios.get(url)
+//   .then(function(response) {
+//     return response.data; // response.data instead of response.json() with fetch
+//   })
+//   .then(function(quote) {
+//     console.log('data decoded from JSON:', quote);
---
+++ src/components/DisplayQuote.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+// const DisplayQuote = (props) => (
+    
+// );
+
+// export default DisplayQuote;
